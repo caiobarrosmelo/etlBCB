ef8c44ebecb5b0967bdb2fd3d998aad18fb56b18
Caio <163363027+caiobarrosmelo@users.noreply.github.com>
2025-04-04 15:55:24 -0300

    Conteúdo da aula 03

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import pandas as pd
 
 
 from src.extractTransform import requestApiBcb
-from src.load import salvarCsv
+from src.load import salvarCsv, salvarSqlite, salvarMySql
 
-dadosBcb = requestApiBcb('20191')
-salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";", ".")
+dadosBcb = requestApiBcb("20191")
+
+# salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";", ".")
+
+# salvarSqlite (dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
+
+salvarMySql(dadosBcb, "root", "root", "localhost", "etlbcb", "meios_pagamentos_tri")
